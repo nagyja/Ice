f29f0688ca027301e4b76c7f2c8a6b712c847057
Jeffrey Nagy <nagyja@gmail.com>
2018-02-16 13:48:15 -0600

    List of events and RSVP static implementation

--- src/containers/home/events_list.js
+++ src/containers/home/events_list.js
@@ -1,9 +1,40 @@
 import React, { Component } from 'react';
+import Events from '../eventList/event_details';
+
+    // const eventItems = props.events.map(Event => {
+    //     return(
+    //         <div>
+    //         {!this.state.noEvents &&  <EventListItem/>: "No events currently scheduled"}
+    //        </div>
+    //     )
+    // });
+
+    // const eventItems = props.Events.map(Event => {
+    //     return(
+    //         <div>
+    //         <Events/>
+    //        </div>
+    //     )
+    // });
 
 class EventList extends Component {
+    // constructor () {
+    //     super()
+    //     this.state = {
+    //         noEvents: true
+    //     }
+    // }
+
+    // componentDidMount(){
+    //     const eventCheck = Events ? this.setState({noEvents: false}) : this.setState({noEvents:true});
+    // }
+  
+    
     render(){
         return(
-            <h2>This is the EventList Element</h2>
+            <div>
+               <Events/>
+            </div>
         );
     }
 }
