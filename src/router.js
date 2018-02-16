eeb43e8790f52e42e635b1e1b3e47d0bb9d265b3
GreedForge <jafecafe@gmail.com>
2018-02-16 11:43:24 -0600

    changed routes.js

--- src/router.js
+++ src/router.js
@@ -4,9 +4,6 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 //Routes
 import LandingPage from './containers/landing_page';
 import HomePage from './containers/home';
-import PeopleList from './containers/home/people_list';
-import EventList from './containers/home/events_list';
-import Profile from './containers/home/profile';
 
 
 const Routes = () => {
@@ -14,9 +11,6 @@ const Routes = () => {
         <BrowserRouter>
             <div>
                 <Switch>
-                    <Route path="/home/profile" component={Profile} />
-                    <Route path="/home/events_list" component={EventList} />
-                    <Route path="/home/people_list" component={PeopleList} />
                     <Route path="/home" component={HomePage} />                    
                     <Route path="/" component={LandingPage} />
                 </Switch>
