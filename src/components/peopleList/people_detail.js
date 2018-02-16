1c0754b6fb1a032baac363e0f1e70687d944fc7f
GreedForge <jafecafe@gmail.com>
2018-02-16 15:00:54 -0600

    added a card to people detail

--- src/components/peopleList/people_detail.js
+++ src/components/peopleList/people_detail.js
@@ -3,7 +3,13 @@ import React, { Component } from 'react';
 class PeopleDetail extends Component {
     render(){
         return(
-            <h1>This is the PeopleDetail Element</h1>
+            <div className="card">
+                <img className="card-image-top" src={"http://lorempixel.com/100/100"} />
+                <div className="card-body">
+                    <h5 className="card-title">Joseph Jaffe</h5>
+                    <p className="card-text">Some random info about myself. Some more random info about myself</p>
+                </div>
+            </div>
         );
     }
 }
